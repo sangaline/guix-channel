3f6811240915ed6688692f357b13eb4e7fad4ce1
Evan Sangaline <esangaline@gmail.com>
2021-08-20 11:29:57 -0400

    Another try at getting hugo to work.

--- sangaline/packages/hugo.scm
+++ sangaline/packages/hugo.scm
@@ -6,33 +6,34 @@
              (guix build-system go))
 
 (let ((commit "7611078daef32306ab31fe360db9895cdd3626d3"))
-    (package (inherit go)
-      (name "hugo")
-      (version (git-version "0.55.6" "1" commit))
-      (source (origin (method git-fetch)
-                      (uri (git-reference
-                            (url "https://github.com/gohugoio/hugo")
-                            (commit commit)))
-                      (file-name (git-file-name name version))
-                      (sha256 (base32
-                               "06yqpcs55gvv1sr368as8k6vy0n5c2fpq7smbfjd8mp0icb31q13"))))
-      (home-page "https://gohugo.io/")
-      (build-system go-build-system)
-      (inputs
-       `(
-         ("go-github-com-burntsushi-locker"
-          ,go-github-com-burntsushi-locker)
-         ("go-github-com-burntsushi-toml"
-          ,go-github-com-burntsushi-toml)
-         ))
-      (arguments
-       `(
-         #:import-path "github.com/gohugoio/hugo"
-         ))
-      (synopsis "A static HTML and CSS website generator written
-in Go.")
-      (description "Hugo takes a directory with content and templates
-and renders them into a full HTML website. It relies on Markdown
-files with front matter for metadata, and you can run it from any
-directory.")
-      (license license:asl2.0)))
+	(define-public hugo
+	    (package (inherit go)
+	      (name "hugo")
+	      (version (git-version "0.55.6" "1" commit))
+	      (source (origin (method git-fetch)
+			      (uri (git-reference
+				    (url "https://github.com/gohugoio/hugo")
+				    (commit commit)))
+			      (file-name (git-file-name name version))
+			      (sha256 (base32
+				       "06yqpcs55gvv1sr368as8k6vy0n5c2fpq7smbfjd8mp0icb31q13"))))
+	      (home-page "https://gohugo.io/")
+	      (build-system go-build-system)
+	      (inputs
+	       `(
+		 ("go-github-com-burntsushi-locker"
+		  ,go-github-com-burntsushi-locker)
+		 ("go-github-com-burntsushi-toml"
+		  ,go-github-com-burntsushi-toml)
+		 ))
+	      (arguments
+	       `(
+		 #:import-path "github.com/gohugoio/hugo"
+		 ))
+	      (synopsis "A static HTML and CSS website generator written
+	in Go.")
+	      (description "Hugo takes a directory with content and templates
+	and renders them into a full HTML website. It relies on Markdown
+	files with front matter for metadata, and you can run it from any
+	directory.")
+	      (license license:asl2.0))))
